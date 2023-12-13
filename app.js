3a79408bb119740617ed486a4e0dde22583f9a51
Donald Mohlala <137010579+ByteBeem@users.noreply.github.com>
2023-12-13 13:35:19 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,21 +1,29 @@
 const socket = io('https://wood-copper-khaan.glitch.me/');
 
+let dynamicBalance = 680.00;
+
 function rollDice() {
   playDiceRollSound();
   const selectedBet = document.getElementById('bet-dropdown').value;
-  
+
+  if(dynamicBalance <= 0 || dynamicBalance < selectedBet){
+    alert("Insufficient balance , please deposit!")
+  }else{
+
+  dynamicBalance -= parseFloat(selectedBet);
+  document.getElementById('balance-amount').textContent = `R${dynamicBalance.toFixed(2)}`;
+
   socket.emit('rollDice', selectedBet);
+}
 }
 
 socket.on('diceRollResult', (randomNumbers) => {
-  
   const dice = [...document.querySelectorAll(".die-list")];
 
   dice.forEach((die, index) => {
     toggleClasses(die);
     die.dataset.roll = randomNumbers[index];
   });
-
 });
 
 function toggleClasses(die) {
@@ -29,3 +37,6 @@ function playDiceRollSound() {
 }
 
 document.getElementById("roll-button").addEventListener("click", rollDice);
+
+
+document.getElementById('balance-amount').textContent = `R${dynamicBalance.toFixed(2)}`;
