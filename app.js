6cf30bed9171f78f31d4112321f6601b906d03f9
Donald Mohlala <137010579+ByteBeem@users.noreply.github.com>
2023-12-13 12:19:48 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 function rollDice() {
+  playDiceRollSound();
   const dice = [...document.querySelectorAll(".die-list")];
   const selectedBet = document.getElementById('bet-dropdown').value;
-  console.log("bet",selectedBet);
+  console.log("bet", selectedBet);
+  
+
+
   dice.forEach(die => {
     toggleClasses(die);
     die.dataset.roll = getRandomNumber(1, 6);
@@ -17,6 +21,11 @@ function getRandomNumber(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function playDiceRollSound() {
+  const audio = new Audio('dice-142528.mp3');
+  audio.play();
 }
 
 document.getElementById("roll-button").addEventListener("click", rollDice);
