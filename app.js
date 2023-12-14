9548799f8d987b67fcbb24c8e3be39c155d2b9e3
Donald Mohlala <137010579+ByteBeem@users.noreply.github.com>
2023-12-14 11:46:46 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,42 +1,69 @@
-const socket = io('https://wood-copper-khaan.glitch.me/');
+document.addEventListener('DOMContentLoaded', () => {
+  const socket = io('https://wood-copper-khaan.glitch.me/');
+  let dynamicBalance = 680.00;
 
-let dynamicBalance = 680.00;
+  // Function to send token to the server
+  function sendTokenToServer() {
+    const storedToken = localStorage.getItem('token');
 
-function rollDice() {
-  playDiceRollSound();
-  const selectedBet = document.getElementById('bet-dropdown').value;
+    if (storedToken) {
+      fetch('https://wood-copper-khaan.glitch.me/verify-token', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          token: storedToken,
+        }),
+      })
+      .then(response => response.json())
+      .then(data => {
+        const { decodedToken, userBalance } = data;
 
-  if(dynamicBalance <= 0 || dynamicBalance < selectedBet){
-    alert("Insufficient balance , please deposit!")
-  }else{
+        // Update UI or perform any other actions based on the user balance
+        dynamicBalance = userBalance;
+        document.getElementById('balance-amount').textContent = `R${dynamicBalance.toFixed(2)}`;
+      })
+      .catch(error => {
+        console.error('Error sending token to server:', error);
+      });
+    }
+  }
 
-  dynamicBalance -= parseFloat(selectedBet);
-  document.getElementById('balance-amount').textContent = `R${dynamicBalance.toFixed(2)}`;
+  // Call the function to send token when the page loads
+  sendTokenToServer();
 
-  socket.emit('rollDice', selectedBet);
-}
-}
+  function rollDice() {
+    playDiceRollSound();
+    const selectedBet = document.getElementById('bet-dropdown').value;
 
-socket.on('diceRollResult', (randomNumbers) => {
-  const dice = [...document.querySelectorAll(".die-list")];
+    if (dynamicBalance <= 0 || dynamicBalance < selectedBet) {
+      alert("Insufficient balance, please deposit!");
+    } else {
+      dynamicBalance -= parseFloat(selectedBet);
+      document.getElementById('balance-amount').textContent = `R${dynamicBalance.toFixed(2)}`;
+      socket.emit('rollDice', selectedBet);
+    }
+  }
 
-  dice.forEach((die, index) => {
-    toggleClasses(die);
-    die.dataset.roll = randomNumbers[index];
+  socket.on('diceRollResult', (randomNumbers) => {
+    const dice = [...document.querySelectorAll(".die-list")];
+
+    dice.forEach((die, index) => {
+      toggleClasses(die);
+      die.dataset.roll = randomNumbers[index];
+    });
   });
+
+  function toggleClasses(die) {
+    die.classList.toggle("odd-roll");
+    die.classList.toggle("even-roll");
+  }
+
+  function playDiceRollSound() {
+    const audio = new Audio('dice-142528.mp3');
+    audio.play();
+  }
+
+  document.getElementById("roll-button").addEventListener("click", rollDice);
 });
-
-function toggleClasses(die) {
-  die.classList.toggle("odd-roll");
-  die.classList.toggle("even-roll");
-}
-
-function playDiceRollSound() {
-  const audio = new Audio('dice-142528.mp3');
-  audio.play();
-}
-
-document.getElementById("roll-button").addEventListener("click", rollDice);
-
-
-document.getElementById('balance-amount').textContent = `R${dynamicBalance.toFixed(2)}`;
